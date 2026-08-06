6e2faaf886a9eddc3471668d07f9ac09e805385a
pershinikita2002@gmail.com <pershinikita2002@gmail.com>
2026-08-06 13:41:42 +0700

    Add product routes for view, create and delete

--- src/main/java/com/marketHub/marketplace/controllers/ProductController.java
+++ src/main/java/com/marketHub/marketplace/controllers/ProductController.java
@@ -1,13 +1,49 @@
 package com.marketHub.marketplace.controllers;
 
+import com.marketHub.marketplace.models.Product;
+import com.marketHub.marketplace.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    // нулевой старт сайта
     @GetMapping("/")
-    public String products() {
-        return "pruducts";
+    public String products(Model model) {
+        model.addAttribute("products", productService.listProducts());
+        return "products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model){
+        model.addAttribute("product", productService.getProductById(id));
+        return "product-info";
+    }
+
+
+    // кнопка создать
+    @PostMapping("/product/create")
+    public String createProduct(Product product) {
+        productService.saveProduct(product);
+        // выкинуть на глваную
+        return "redirect:/";
+    }
+
+
+    //кнопка удалить
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProducts(id);
+        //выкинуть на главную
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/marketHub/marketplace/services/ProductService.java
+++ src/main/java/com/marketHub/marketplace/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.marketHub.marketplace.services;
 
 import com.marketHub.marketplace.models.Product;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,8 +10,10 @@ import java.util.List;
 @Service
 public class ProductService {
     private List<Product> products = new ArrayList<>();
+    private long ID = 0;
     {
         products.add(new Product(
+                ++ID,
                 "PlayStation 5",
                 "Simple description",
                 67000,
@@ -18,10 +21,31 @@ public class ProductService {
                 "tomas"));
 
         products.add(new Product(
-                "PlayStation 5",
+                ++ID,
+                "Apple",
                 "Simple description",
-                67000,
-                "Krasnoyarsk",
-                "tomas"));
+                59000,
+                "Miass",
+                "Apple"));
+    }
+
+    public List<Product> listProducts() {
+        return products;
+    }
+
+    public void saveProduct(Product product) {
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProducts(Long id){
+        products.removeIf(product -> product.getId().equals(id));
+    }
+
+    public Product getProductById(Long id) {
+        for (Product product : products){
+            if (product.getId().equals(id)) return  product;
+        }
+        return null;
     }
 }
